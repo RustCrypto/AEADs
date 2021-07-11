c556954ec4e0e2463ae98aa33c3d060f601f1fdb
Artyom Pavlov <newpavlov@gmail.com>
2021-07-11 02:25:59 +0000

    mgm: RFC 9058 test vectors (#335)

---
+++ mgm/tests/bad_nonce.rs
@@ -0,0 +1,22 @@
+//! Tests for nonce validity checks
+use aead::{generic_array::GenericArray, Aead, NewAead};
+use mgm::Mgm;
+
+#[test]
+fn kuznyechik_bad_nonce() {
+    let key = GenericArray::from_slice(&[0u8; 32]);
+    let mut nonce = GenericArray::clone_from_slice(&[0u8; 16]);
+    let cipher = Mgm::<kuznyechik::Kuznyechik>::new(key);
+    let mut enc_data = cipher.encrypt(&nonce, &[][..]).unwrap();
+    let res = cipher.decrypt(&nonce, &enc_data[..]);
+    assert!(res.is_ok());
+    enc_data[0] ^= 0x80;
+    let res = cipher.decrypt(&nonce, &enc_data[..]);
+    assert!(res.is_err());
+
+    nonce[0] ^= 0x80;
+    let res = cipher.encrypt(&nonce, &[][..]);
+    assert!(res.is_err());
+    let res = cipher.decrypt(&nonce, &enc_data[..]);
+    assert!(res.is_err());
+}

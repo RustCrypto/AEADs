2bd18a108f61123a34b0f457387b6b884bcc4fa0
jake <77554505+brxken128@users.noreply.github.com>
2023-03-18 21:17:23 +0000

    only use criterion-CPB on x86 and x86_64 (#515)

--- benches/src/chacha20poly1305.rs
+++ benches/src/chacha20poly1305.rs
@@ -1,12 +1,16 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use criterion_cycles_per_byte::CyclesPerByte;
 
 use chacha20poly1305::aead::{Aead, KeyInit};
 use chacha20poly1305::ChaCha20Poly1305;
 
 const KB: usize = 1024;
 
-fn bench(c: &mut Criterion<CyclesPerByte>) {
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
+type Benchmarker = Criterion;
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+type Benchmarker = Criterion<criterion_cycles_per_byte::CyclesPerByte>;
+
+fn bench(c: &mut Benchmarker) {
     let mut group = c.benchmark_group("chacha20poly1305");
 
     for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
@@ -27,9 +31,18 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
     group.finish();
 }
 
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
+criterion_group!(
+    name = benches;
+    config = Criterion::default();
+    targets = bench
+);
+
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
 criterion_group!(
     name = benches;
     config = Criterion::default().with_measurement(CyclesPerByte);
     targets = bench
 );
+
 criterion_main!(benches);

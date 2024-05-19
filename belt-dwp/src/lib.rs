a114811d3249ae423879ab36259fd5c1ee23d2c2
Alexandr Kitaev <makavity231@gmail.com>
2024-05-19 16:09:56 +0300

    fix belt-dwp tests

--- belt-dwp/src/lib.rs
+++ belt-dwp/src/lib.rs
@@ -19,9 +19,9 @@
 //!     BeltDwp, Nonce
 //! };
 //!
-//! let key = BeltDwp::generate_key()?;
+//! let key = BeltDwp::generate_key(&mut OsRng);
 //! let cipher = BeltDwp::new(&key);
-//! let nonce = BeltDwp::generate_nonce()?; // 96-bits; unique per message
+//! let nonce = BeltDwp::generate_nonce(&mut OsRng); // 128-bits; unique per message
 //! let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref())?;
 //! let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref())?;
 //! assert_eq!(&plaintext, b"plaintext message");
@@ -60,7 +60,7 @@
 //!
 //! let key = BeltDwp::generate_key()?;
 //! let cipher = BeltDwp::new(&key);
-//! let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+//! let nonce = Nonce::from_slice(b"unique nonce1234"); // 128-bits; unique per message
 //!
 //! let mut buffer: Vec<u8, 128> = Vec::new(); // Note: buffer needs 16-bytes overhead for auth tag
 //! buffer.extend_from_slice(b"plaintext message");

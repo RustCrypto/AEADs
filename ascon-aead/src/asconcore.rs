ddaf7aed3f836cb85854fb8b7df05b14a2c69bb2
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-03-03 18:25:50 +0300

    Add `#[allow(non_local_definitions)]`

--- ascon-aead/src/asconcore.rs
+++ ascon-aead/src/asconcore.rs
@@ -56,6 +56,7 @@ pub(crate) trait InternalKey<KS: ArrayLength<u8>>:
     fn get_k2(&self) -> u64;
 }
 
+#[allow(non_local_definitions)]
 #[derive(Clone)]
 #[cfg_attr(feature = "zeroize", derive(zeroize::Zeroize, zeroize::ZeroizeOnDrop))]
 pub(crate) struct InternalKey16(u64, u64);

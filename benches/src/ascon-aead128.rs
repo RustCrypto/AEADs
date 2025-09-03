94366f1c17641eea1ee67ac29bb8f6943a37a463
Tony Arcieri <bascule@gmail.com>
2025-09-02 21:31:37 -0600

    Bump universal hashes
    Uses `hybrid-array` v0.4-compatible versions

--- benches/src/ascon-aead128.rs
+++ benches/src/ascon-aead128.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
-use ascon_aead::aead::{AeadInPlaceDetached, KeyInit};
-use ascon_aead::AsconAead128;
+use ascon_aead128::aead::{AeadInOut, KeyInit};
+use ascon_aead128::AsconAead128;
 
 const KB: usize = 1024;
 
@@ -10,7 +10,7 @@ type Benchmarker = Criterion;
 #[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
 type Benchmarker = Criterion<criterion_cycles_per_byte::CyclesPerByte>;
 
-fn bench<A: AeadInPlaceDetached + KeyInit>(name: &str, c: &mut Benchmarker) {
+fn bench<A: AeadInOut + KeyInit>(name: &str, c: &mut Benchmarker) {
     let mut group = c.benchmark_group(name);
     let nonce = black_box(Default::default());
     let cipher = black_box(A::new(&Default::default()));
@@ -18,15 +18,17 @@ fn bench<A: AeadInPlaceDetached + KeyInit>(name: &str, c: &mut Benchmarker) {
     let mut buf = vec![0u8; 16 * KB];
     for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
         let buf = &mut buf[..size];
-        let tag = cipher.encrypt_in_place_detached(&nonce, b"", buf).unwrap();
+        let tag = cipher
+            .encrypt_inout_detached(&nonce, b"", buf.into())
+            .unwrap();
 
         group.throughput(Throughput::Bytes(size as u64));
 
         group.bench_function(BenchmarkId::new("encrypt-128", size), |b| {
-            b.iter(|| cipher.encrypt_in_place_detached(&nonce, b"", buf))
+            b.iter(|| cipher.encrypt_inout_detached(&nonce, b"", buf.into()))
         });
         group.bench_function(BenchmarkId::new("decrypt-128", size), |b| {
-            b.iter(|| cipher.decrypt_in_place_detached(&nonce, b"", buf, &tag))
+            b.iter(|| cipher.decrypt_inout_detached(&nonce, b"", buf.into(), &tag))
         });
     }
 
